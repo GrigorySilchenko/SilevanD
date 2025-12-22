f07ab5559de357b6dac72a67d9aadfd089439ee6
Grigory <rygarock@gmail.com>
2025-12-22 15:13:02 +0300

    23. add id in admin act_creation sticker_place

--- SilEvAnd/act_creation/admin.py
+++ SilEvAnd/act_creation/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import Registry, Manufacturer, StickPlace, Boss, Conformity, Act, RegistryModify
-
+from application.admin import StatusAdmin
 
 admin.site.register(Registry)
 admin.site.register(RegistryModify)
 admin.site.register(Manufacturer)
-admin.site.register(StickPlace)
+admin.site.register(StickPlace, StatusAdmin)
 admin.site.register(Boss)
 admin.site.register(Conformity)
 admin.site.register(Act)
